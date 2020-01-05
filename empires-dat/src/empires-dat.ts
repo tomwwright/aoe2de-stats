63d000750ec3f4cc1a741a5d69f343a5baafad82
Tom Wright <tom.suffern.wright@gmail.com>
2020-01-05 17:32:53 +1100

    Add raw dat file parser
    Signed-off-by: Tom Wright <tom.suffern.wright@gmail.com>

---
+++ empires-dat/src/empires-dat.ts
@@ -0,0 +1,211 @@
+import * as fs from "fs";
+import { RawEmpiresDat, RawCivilization, RawCombatObject, RawBuildingObject } from "./raw-empires-dat";
+import { EmpiresStrings } from "./empires-strings";
+
+type Unit = {
+  id: number;
+  type: number;
+  name: string;
+  hitPoints: number;
+  lineOfSight: number;
+  garrisonCapacity: number;
+  speed: number;
+  turnSpeed: number;
+  turnRadius: number;
+  turnRadiusSpeed: number;
+  accuracyDispersion: number;
+  attackSpeed: number;
+  attacks: {
+    [type: string]: number;
+  };
+  armours: {
+    [type: string]: number;
+  };
+  blastRange: number;
+  displayed: {
+    attack: number;
+    meleeArmour: number;
+    pierceArmour: number;
+    range: number;
+  };
+  flankAttackModifier: number;
+  frameDelay: number;
+  range: {
+    minimum: number;
+    maximum: number;
+  };
+  resourceCosts: {
+    [type: string]: number;
+  };
+  rearAttackModifier: number;
+};
+
+type Building = Unit & {
+  garrisonType: number;
+  garrisonHealRate: number;
+  garrisonRepairRate: number;
+};
+
+type Civilisation = {
+  name: string;
+  iconSetId: number;
+  techTreeId: number;
+  teamBonusId: number;
+  units: Unit[];
+  buildings: Building[];
+};
+
+export class Parser {
+  private strings: { [id: string]: string };
+
+  private lookups = {
+    resourceTypes: {
+      0: "FOOD",
+      1: "WOOD",
+      2: "STONE",
+      3: "GOLD",
+      4: "POPULATION"
+    } as { [type: number]: string },
+    armourClasses: {
+      1: "Infantry",
+      2: "Turtle Ships",
+      3: "Base Pierce",
+      4: "Base Melee",
+      5: "War Elephants",
+      8: "Cavalry",
+      11: "All Buildings",
+      13: "Stone Defense",
+      14: "Predator Animals",
+      15: "Archers",
+      16: "Ships and Saboteurs",
+      17: "Rams",
+      18: "Trees",
+      19: "Unique Units",
+      20: "Siege Weapons",
+      21: "Standard Buildings",
+      22: "Walls and Gates",
+      23: "Gunpowder Units",
+      24: "Boars",
+      25: "Monks",
+      26: "Castles",
+      27: "Spearmen",
+      28: "Cavalry Archers",
+      29: "Eagle Warriors",
+      30: "Camels",
+      31: "Leitis attack",
+      32: "Condottieri",
+      33: "Organ Gun Projectiles",
+      34: "Fishing Ships",
+      35: "Mamelukes",
+      36: "Heroes & Kings"
+    } as { [type: number]: string }
+  };
+
+  constructor(strings: { [id: string]: string }) {
+    this.strings = strings;
+  }
+
+  parse(data: RawEmpiresDat) {
+    const parsed = {
+      civilisations: [] as Civilisation[]
+    };
+
+    for (const raw of data.civilizations) {
+      parsed.civilisations.push(this.parseCivilisation(raw));
+    }
+
+    return parsed;
+  }
+
+  private parseCivilisation(raw: RawCivilization): Civilisation {
+    const civ = {
+      name: raw.name.value,
+      iconSetId: raw.iconSet,
+      techTreeId: raw.techTreeId,
+      teamBonusId: raw.teamBonusId,
+      units: raw.objects.filter(obj => obj.type == 70).map(obj => this.parseUnit(obj as RawCombatObject)),
+      buildings: raw.objects.filter(obj => obj.type == 80).map(obj => this.parseBuilding(obj as RawBuildingObject))
+    };
+
+    return civ;
+  }
+
+  private parseUnit(raw: RawCombatObject): Unit {
+    const unit = {
+      id: raw.id,
+      type: raw.type,
+      name: this.strings[raw.languageDllName],
+      hitPoints: raw.hitPoints,
+      lineOfSight: raw.lineOfSight,
+      garrisonCapacity: raw.garrisonCapacity,
+      speed: raw.speed,
+      turnSpeed: raw.turnSpeed,
+      turnRadius: raw.turnRadiusSpeed,
+      turnRadiusSpeed: raw.turnRadiusSpeed,
+      accuracyDispersion: raw.accuracyDispersion,
+      attackSpeed: raw.attackSpeed,
+      attacks: raw.attacks.reduce((obj, v) => {
+        obj[this.lookups.armourClasses[v.type]] = v.amount;
+        return obj;
+      }, {} as { [type: string]: number }),
+      armours: raw.armors.reduce((obj, v) => {
+        obj[this.lookups.armourClasses[v.type]] = v.amount;
+        return obj;
+      }, {} as { [type: string]: number }),
+      blastRange: raw.blastRange,
+      displayed: {
+        attack: raw.attackDisplayed,
+        meleeArmour: raw.meleeArmorDisplayed,
+        pierceArmour: raw.pierceArmorDisplayed,
+        range: raw.rangeDisplayed
+      },
+      flankAttackModifier: raw.flankAttackModifier,
+      frameDelay: raw.frameDelay,
+      range: {
+        minimum: raw.weaponRangeMin,
+        maximum: raw.weaponRangeMax
+      },
+      resourceCosts: raw.resourceCost.reduce((obj, v) => {
+        obj[this.lookups.resourceTypes[v.type]] = v.amount;
+        return obj;
+      }, {} as { [type: string]: number }),
+      rearAttackModifier: raw.rearAttackModifier
+    };
+
+    return unit;
+  }
+
+  private parseBuilding(raw: RawBuildingObject): Building {
+    const unit = this.parseUnit(raw);
+
+    const building = {
+      ...unit,
+      garrisonType: raw.garrisonType,
+      garrisonHealRate: raw.garrisonHealRate,
+      garrisonRepairRate: raw.garrisonRepairRate
+    };
+
+    return building;
+  }
+}
+
+// main
+if (require.main === module) {
+  console.log("== Age of Empires 2: Definitive Edition raw data parser ==");
+  const inputDatFilename = process.argv[2] || "dat/empires2_x2_p1.dat.json";
+  const inputStringsFilename = process.argv[3] || "dat/empires-strings.json";
+  const outputFilename = process.argv[4] || "dat/empires-dat.json";
+
+  console.log(`Input Dat: ${inputDatFilename}\nInput Strings: ${inputStringsFilename}\nOutput: ${outputFilename}`);
+
+  const empiresDat: RawEmpiresDat = JSON.parse(fs.readFileSync(inputDatFilename).toString());
+  const empiresStrings: EmpiresStrings = JSON.parse(fs.readFileSync(inputStringsFilename).toString());
+
+  const parser = new Parser(empiresStrings.strings);
+
+  const parsed = parser.parse(empiresDat);
+
+  console.log(`Civilisations: ${parsed.civilisations.length}`);
+
+  fs.writeFileSync(outputFilename, JSON.stringify(parsed));
+}
