86bb5b1ca7346e8622f297bd4a1475bbf3e5d789
Tom Wright <tom.suffern.wright@gmail.com>
2020-01-14 08:23:54 +1100

    Denormalise techs to have a row per effect

--- empires-stats-app/src/TechList.tsx
+++ empires-stats-app/src/TechList.tsx
@@ -2,9 +2,22 @@ import React, { useContext } from "react";
 
 import { EmpiresDatContext } from "./EmpiresDatContext";
 import { DataTable } from "./DataTable";
+import { Tech } from "../../empires-dat/src/empires-dat";
 
 export const TechList: React.StatelessComponent<{}> = () => {
   const empires = useContext(EmpiresDatContext);
 
-  return <DataTable data={empires.techs} />;
+  const denormalisedTechs = denormaliseByEffects(empires.techs);
+
+  return <DataTable data={denormalisedTechs} />;
+};
+
+const denormaliseByEffects = (techs: Tech[]) => {
+  return techs.flatMap((tech, i) =>
+    tech.effects.map(effect => ({
+      techId: i,
+      name: tech.name,
+      ...effect
+    }))
+  );
 };
